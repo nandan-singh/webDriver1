29e381fe525ed48498cc4d5ba209f4887653806c
nandan singh <nandan_singh@live.com>
2013-10-03 00:40:40 +0530

    my first commit

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,18 @@
+
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MyFirstTest {
+	
+	@Test
+	public void firstTestCase()
+	{
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("http://www.google.com");
+		
+	}
+
+}
